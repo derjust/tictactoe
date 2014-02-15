438181ad905f9e2286fef20f326255243216f6ef
Martin Steiger <git@martin-steiger.de>
2014-02-15 12:32:43 +0100

    Updated rendering

--- web/DartTIcTacToe/web/darttictactoe.dart
+++ web/DartTIcTacToe/web/darttictactoe.dart
@@ -7,19 +7,20 @@ library sunflower;
 import 'dart:html';
 import 'dart:math';
 
-const String ORANGE = "orange";
-const int SEED_RADIUS = 30;
-const int BOX_SIZE = 100;
-const int MAX_D = 300;
+const int MAX_D = 400;
 const int rows = 3;
 const int cols = 3;
+const num BOX_SIZE = MAX_D / 3;
 const num centerX = MAX_D / 2;
 const num centerY = centerX;
+const num CROSS_RADIUS = BOX_SIZE * 0.4 - 10;
+const num CIRCLE_RADIUS = BOX_SIZE * 0.4 - 10;
 
 final CanvasRenderingContext2D context =
   (querySelector("#canvas") as CanvasElement).context2D;
 
 void main() {
+  (querySelector("#canvas") as CanvasElement)..width = MAX_D..height = MAX_D;
   draw();
 }
 
@@ -35,16 +36,16 @@ void draw() {
     drawLine(c * BOX_SIZE, 0, c * BOX_SIZE, MAX_D);    
   }
 
-  drawSeed(1 * BOX_SIZE, 2 * BOX_SIZE);
+  drawCircle(1 * BOX_SIZE, 2 * BOX_SIZE);
   drawCross(0 * BOX_SIZE, 1 * BOX_SIZE);
 }
 
 /// Draw a small circle representing a seed centered at (x,y).
-void drawSeed(num x, num y) {
+void drawCircle(num x, num y) {
   context..beginPath()
          ..lineWidth = 10
          ..strokeStyle = "BLUE"
-         ..arc(x + BOX_SIZE / 2, y + BOX_SIZE / 2, SEED_RADIUS, 0, 2 * PI, false)
+         ..arc(x + BOX_SIZE / 2, y + BOX_SIZE / 2, CIRCLE_RADIUS, 0, 2 * PI, false)
          ..closePath()
          ..stroke();
 }
@@ -56,11 +57,11 @@ void drawCross(num x, num y) {
   y = y + BOX_SIZE / 2;
   context..beginPath()
          ..lineWidth = 10
-         ..strokeStyle = ORANGE
-         ..moveTo(x - SEED_RADIUS / 2, y - SEED_RADIUS / 2)
-         ..lineTo(x + SEED_RADIUS / 2, y + SEED_RADIUS / 2)
-         ..moveTo(x - SEED_RADIUS / 2, y + SEED_RADIUS / 2)
-         ..lineTo(x + SEED_RADIUS / 2, y - SEED_RADIUS / 2)
+         ..strokeStyle = "ORANGE"
+         ..moveTo(x - CROSS_RADIUS, y - CROSS_RADIUS)
+         ..lineTo(x + CROSS_RADIUS, y + CROSS_RADIUS)
+         ..moveTo(x - CROSS_RADIUS, y + CROSS_RADIUS)
+         ..lineTo(x + CROSS_RADIUS, y - CROSS_RADIUS)
          ..closePath()
          ..stroke();
 }
