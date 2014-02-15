ce7e9481ce358e3ff06befb1de492b66a6860bda
Sebastian Just <Sebastian.Just@sungard.com>
2014-02-15 13:20:21 +0100

    added client stub

---
+++ web/DartTIcTacToe/lib/Client.dart
@@ -0,0 +1,31 @@
+
+
+class Client {
+  
+  int playerid;
+  int gameid;
+  
+  int createPlayer(String name) {
+    
+  }
+  
+  
+  int createGame() {
+    
+  }
+  
+  joinGame(int  gameId) {
+    
+  }
+  
+  List<int> listGames() {
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+}
